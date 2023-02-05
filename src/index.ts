fdfd8c092dee212bdaa4bc5d69883c391ed82fef
KillerBossOriginal <killerbossoriginal@outlook.it>
2023-02-05 13:38:01 +0100

    2023.02.05

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { getTime } from '@thundernetworkrad/time';
+import { log } from '@thundernetworkrad/logs';
+import { cin } from 'node-cin';
+import { createCout } from 'node-cout';
+
+export default {
+    getTime,
+    log,
+    cin,
+    createCout
+}
